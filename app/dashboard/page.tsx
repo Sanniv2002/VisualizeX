e415fd70384536827307880e90cb7502d136b81a
Sanniv <sanniv.1020@gmail.com>
2024-04-10 15:42:34 +0530

    init

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,7 +5,7 @@ import Projects from "@/components/Projects"
 import { useSession } from "next-auth/react"
 
 export default async function Page(){
-    const { data: session } = useSession()
+    const { data: session } = await useSession()
     return <div className="bg-gray-900 h-screen overflow-y-auto">
         {Header("/ dashboard", session)}
         <div className="px-40 py-20">
