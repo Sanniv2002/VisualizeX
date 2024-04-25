2feed21e4cbfa7c5ef01a7245aa65a007135de42
Sanniv <sanniv.1020@gmail.com>
2024-04-25 17:19:34 +0530

    added redirect from home if user did not log out

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Header from "@/components/RouteHeader"
 import Hero from "@/components/Hero";
 import Herobuttons from "@/components/Herobuttons";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./lib/auth";
+import { redirect } from "next/navigation";
 
-
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+  if(session){
+    redirect("/dashboard")
+  }
   return (
     <main className="flex flex-col bg-gray-900 h-screen w-screen overflow-y-hidden overflow-x-clip">
       {Header("", null)}
